3319240793d28d908ba7c589242f96df398783b1
Iago Caires <cairesiago07@gmail.com>
2020-07-24 22:12:30 -0300

    Implementei umas idéias que tive
    Implementei algumas configurações como não salvar um Todo em branco e também implementei a redenrização em tela.

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
-let listaAtividades = JSON.parse(localStorage.getItem('list_atividade')) || [];
+let listTodos = JSON.parse(localStorage.getItem('listTodos')) || [];
 
-let inputName = document.querySelector('#nome');
-let inputData = document.querySelector('#data');
-let inputHoraInicio = document.querySelector('#horaInicio');
-let inputHoraFim = document.querySelector('#horaFim');
+function createTodo(){
+    let getVerification = document.querySelector('#cadastrarTodo');
 
-
-
-function addAtividade(){
-    let dados = [inputName.value, inputData.value, inputHoraInicio.value, inputHoraFim.value];
-
-    listaAtividades.push(dados);
-    saveToStorage();
-}
-
-function renderAtividade(){
-    for(dado of listaAtividades){
-        let sectionElement = document.querySelector('section');
- 
-        let divElelment = document.createElement('div');
-        divElelment.setAttribute('id', 'listaDeAtividades');
+    if(getVerification){
+        alert('Por favor, adicione um Todo por ver.');
+    }else{
+        let loadElements = document.querySelector('.container');
+        let sectionElement = document.createElement('section');
         let formElement = document.createElement('form');
-        let ulElement = document.createElement('ul');
-        let liElement = document.createElement('li');
-        let textElement = document.createTextNode(`Atividade: ${dado[0]} | Data: ${dado[1]}
-        | Inicio: ${dado[2]} | Fim: ${dado[3]}`);
-    
-        liElement.appendChild(textElement);
-        ulElement.appendChild(liElement);
-        formElement.appendChild(ulElement);
-        divElelment.appendChild(formElement);
-        sectionElement.appendChild(divElelment);
+        formElement.setAttribute('id', 'cadastrarTodo');
+        let inputElement = document.createElement("input");
+        inputElement.setAttribute('placeholder', 'Title');
+        let divButtons = document.createElement('div');
+        divButtons.setAttribute('id', 'buttonsTodo');
+        let btSalvar = document.createElement('input');
+        btSalvar.setAttribute('value', 'Salvar');  
+        btSalvar.setAttribute('type', 'button');
+        btSalvar.setAttribute('onclick', 'addTodo()');
+        let btCancelar = document.createElement('input');
+        btCancelar.setAttribute('type', 'button');
+        btCancelar.setAttribute('value', 'Cancelar');
+        btCancelar.setAttribute('onclick', 'cancelTodo()');
+
+        divButtons.appendChild(btCancelar);
+        divButtons.appendChild(btSalvar);
+
+        formElement.appendChild(inputElement);
+
+        sectionElement.appendChild(formElement);
+        sectionElement.appendChild(divButtons);
+
+        loadElements.appendChild(sectionElement);
     }
 }
 
+function addTodo(){
+    let inputValeu = document.querySelector('#cadastrarTodo input');
+    
+    if(inputValeu.value.length === 0){
+        alert('[ERROR] Por favor, preencha os campos antes de salvar um Todo.');
+    }else{
+    listTodos.push(inputValeu.value);
+    inputValeu.value = '';
+    saveToStorage();
+    renderTodo();
+    }
+}
+
+function cancelTodo(){
+    let containerElement = document.querySelector('.container');
+
+    containerElement.innerHTML = '';
+}
+
 function saveToStorage(){
-    localStorage.setItem('list_atividade', JSON.stringify(listaAtividades));
+    localStorage.setItem('listTodos', JSON.stringify(listTodos));
+}
+
+function renderTodo(){
+
+    if(listTodos.length === 0){
+        alert('[ATENÇÂO] Você ainda não possui nenhum Todo salvo.');
+    }
+
+    let ulElement = document.createElement('ul');
+    let containerElement = document.querySelector('.container');
+    let sectionElement = document.createElement('section');
+    sectionElement.setAttribute('id', 'exibe-todo');
+  
+    listTodos.map((dados) =>{
+        let liElement = document.createElement('li');
+        let textList = document.createTextNode(dados);
+
+        containerElement.innerHTML = '';
+
+        sectionElement.appendChild(ulElement);
+
+        liElement.appendChild(textList);
+        ulElement.appendChild(liElement)
+
+        sectionElement.appendChild(ulElement);
+        containerElement.appendChild(sectionElement);
+    });
 }
